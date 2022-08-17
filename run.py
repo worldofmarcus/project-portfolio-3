3cb6a981ec2dd909f7bde3e56933989393de4dc4
worldofmarcus <me@marcuseriksson.com>
2022-08-17 15:25:10 +0000

    Setup connection to google sheet and confirm access

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+
+CREDS = Credentials.from_service_account_file("credentials.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("music_collection")
+collection = SHEET.worksheet("collection")
+
+data = collection.get_all_values()
+
+print(data)
