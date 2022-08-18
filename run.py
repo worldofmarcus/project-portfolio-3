d777276719036b9d18f7493158b8b37cacdecb6f
worldofmarcus <me@marcuseriksson.com>
2022-08-18 14:03:05 +0000

    Change name on credentials.txt in run.py

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive",
 ]
 
-CREDS = Credentials.from_service_account_file("credentials.json")
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("music_collection")
